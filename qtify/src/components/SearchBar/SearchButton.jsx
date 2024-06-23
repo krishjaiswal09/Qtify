c6b3fb0b8f411b076491fe1f7eaca9adbd8deee8
Krish Jaiswal <krishjaiswal67890@gmail.com>
2024-06-23 16:14:30 +0530

    updated code

--- qtify/src/components/SearchBar/SearchButton.jsx
+++ qtify/src/components/SearchBar/SearchButton.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import searchIcon from "../../assets/search-icon.svg";
+import Styles from "../SearchBar/SearchButton.module.css"
 
 export default function SearchIcon() {
-    return <img src={searchIcon} alt="logo" width={67} />;
+    return <img className={Styles.searchIcon}src={searchIcon} alt="logo" width={67} />;
 }
